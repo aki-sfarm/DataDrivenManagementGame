ea984e184a71d767d0788445ac8df1de2ff41eaa
aki-sfarm <aki@sfarm.okinawa.jp>
2023-06-23 23:07:02 +0900

    プロジェクト ファイルを追加します。

---
+++ DataDrivenManagementGame/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using System.Data;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace CreateSalesData
+
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            //var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            string dbServerIP = configuration.GetConnectionString("DBServerIP");
+            string databaseName = configuration.GetConnectionString("DatabaseName");
+            string userId = configuration.GetConnectionString("Id");
+
+            string TrustServerCertificate = "True";
+
+
+            var kvUri = configuration.GetConnectionString("KeyVaultURL");
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+            KeyVaultSecret secret = client.GetSecret("devSQLDBpassword");
+
+            string password = secret.Value;
+
+            string connectionString = "Server=" + dbServerIP + ";Database=" + databaseName + ";User Id=" + userId + ";Password=" + password + ";TrustServerCertificate=" + TrustServerCertificate + ";";
+
+
+            var dbConnection = new SqlServerHelper(connectionString);
+
+
+            dbConnection.CreateConnection();
+
+            // Execute the query
+            DataTable customerTable = dbConnection.ExecuteQuery("SELECT * FROM CustomerList");
+
+            // Print out the customer IDs
+            foreach (DataRow row in customerTable.Rows)
+            {
+                Console.WriteLine(row["CustomerID"]);
+            }
+
+            dbConnection.DisposeConnection();
+
+
+        }
+    }
+}
---
+++ DataDrivenManagementGame/SqlServerHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+public class SqlServerHelper
+{
+    private SqlConnection _connection;
+
+    // Connection string should include server name, database name, and authentication details.
+    public string ConnectionString { get; }
+
+    public SqlServerHelper(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+
+    // Method to create connection
+    public void CreateConnection()
+    {
+        if (_connection != null && _connection.State == System.Data.ConnectionState.Open)
+            return;
+
+        _connection = new SqlConnection(ConnectionString);
+        _connection.Open();
+    }
+
+    // Method to execute a query and return the result as a DataTable
+    public DataTable ExecuteQuery(string query)
+    {
+        using var command = new SqlCommand(query, _connection);
+        using var dataAdapter = new SqlDataAdapter(command);
+
+        var resultTable = new DataTable();
+        dataAdapter.Fill(resultTable);
+
+        return resultTable;
+    }
+
+
+
+    // Method to dispose connection
+    public void DisposeConnection()
+    {
+        if (_connection != null && _connection.State == System.Data.ConnectionState.Open)
+            _connection.Close();
+        _connection?.Dispose();
+    }
+}
